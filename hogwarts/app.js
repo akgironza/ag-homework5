59c038e78ab8b077f8bad0b3d64a44f1c3ead301
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-05-20 08:01:56 -0400

    Commit 2 - I added more elements. My power grows with my dedication and hard work

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -8,7 +8,7 @@ $(() => {
 	// put jQuery in here
 });
 
-// Year 1
+// YEAR 1
 const $body = $("body")
 const $container = $("<div>")
 $container.attr('id', 'container')
@@ -21,6 +21,20 @@ $($container).append($h1)
 // console.log($h1)
 
 
+// YEAR 2
+const $h2 = $("<h2>")
+const $h3 = $("<h3>")
+const $h4_1 = $("<h4>")
+const $h4_2 = $("<h4>")
+$h2.text('Arielle')
+$h3.text('Ravenclaw')
+$h4_1.text('Zoe').attr('class', 'cat')
+$h4_2.text('Holly Wand with Unicorn Hair Core')
+$($container).append($h2).append($h3).append($h4_1).append($h4_2)
+
+// console.log($h2)
+// console.log($h3)
+// console.log($h4)
 
 
 
