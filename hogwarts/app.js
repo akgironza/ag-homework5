bbb438e01824dc3f55c4295c2d16bd0035154622
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-05-20 07:26:34 -0400

    Commit 1 - I added my first element and it was fun

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -4,4 +4,24 @@ if (typeof jQuery == 'undefined'){
   } else {console.log('I did it! I linked jQuery and this js file!')};
 */
 
+$(() => {
+	// put jQuery in here
+});
+
+// Year 1
+const $body = $("body")
+const $container = $("<div>")
+$container.attr('id', 'container')
+//console.log($container)
+
+const $h1 = $("<h1>")
+$h1.text('Hogwarts')
+$($body).append($container)
+$($container).append($h1)
+// console.log($h1)
+
+
+
+
+
 
