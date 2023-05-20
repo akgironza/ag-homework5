45186355456318fa8886731879284110734ccfec
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-05-20 08:47:03 -0400

    Commit 5 - The days go by slowly and the years go by too fast

--- hogwarts/app.js
+++ hogwarts/app.js
@@ -50,3 +50,16 @@ $($container).append($ul)
 
 console.log($ul)
 
+// YEAR 4
+// const $h5 = $("<h5>").text("Spring 2017")
+// coming back to this if I have time bc I started my homework late again
+
+// YEAR 5
+$($h4_2).remove()
+const $h4_3 = $("<h4>").text('Holly Wand with Unicorn Hair Core but different from the broken one')
+$($container).append($h4_3)
+$($container).append($ul)
+$h4_3.css("color", "indigo")
+// Send your pet on a spy mission (remove your pet from the DOM, put it somewhere else in your HTML). Make sure your pet's leash stays in your trunk (list item with the same class as your pet inside unordered list)
+// Have your pet come back (remove your pet from the DOM, put it back in its original location)
+// will come back to this if I have time, sounds funny
